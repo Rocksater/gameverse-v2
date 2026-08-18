f261637a6905f481ccff54e9409d44e8ab70e68a
unknown <okutaandycole@gmail.com>
2026-08-18 14:24:57 +0300

    added package-lock.json

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './services/queryClient.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
